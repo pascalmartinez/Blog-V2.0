4c7702de07a1648fe4fcba182c4c04a32da042db
LyraKuroneko <indigo.laetitia@gmail.com>
2018-02-15 10:30:45 +0100

    pagination test

--- database/pagination.php
+++ database/pagination.php
@@ -19,33 +19,36 @@
             $start = $offset + 1;
             $end = min(($offset + $limit), $totalArticle);
 
-            /*echo '<li class="page-item">
-                        <a class="page-link" href="'. ($page - 1) . '" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
+
+            $listdebut = '<li class="page-item">';
+            $listSuite = ($page > 1) ?
+                       ' <a class="page-link" href="?page='.($page - 1).'" title="Previous page" aria-label="Previous">' :
+                            '<span aria-hidden="true">&laquo;</span>
                             <span class="sr-only">Previous</span>
                         </a>
                     </li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                        <a class="page-link" href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                            <span class="sr-only">Next</span>
+                    <li class="page-item"><a class="page-link" href="?page=1">&laquo;</a></li>
+                    <li class="page-item">';
+            $listFin =  ($page < $pages) ? '<a class="page-link" href="?page=' . ($page + 1) . '" title="Next page" aria-label="Next">' :
+                            '<span class="disabled" aria-hidden="true">&raquo;</span>
+                            <span class="disabled sr-only">Next</span>
                         </a>
-                    </li>';*/
-            $previousLink = ($page > 1) ? '<a classe="page-link" href="?page=1" title="First page">&laquo;</a>
+                    </li>';
+
+                    echo $listdebut.$listSuite.$listFin;
+
+           /* $previousLink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a>
             <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : 
             '<span class="disabled">&laquo;</span>
             <span class="disabled">&lsaquo;</span>';
-            
+
             $nextLink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a>
             <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' : 
             '<span class="disabled">&rsaquo;</span>
             <span class="disabled">&raquo;</span>';
 
             echo '<div id="paging"><p>', $previousLink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $totalArticle, ' results ', $nextLink, ' </p></div>';
-
+*/
             return $offset;
         }
         catch (Exception $e) {
