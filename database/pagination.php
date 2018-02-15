c85c260c3420aa4387c55480e60787bdc1025761
Pascal <pmartinez31140@gmail.com>
2018-02-15 14:04:48 +0100

    refactorisation v1.0

--- database/pagination.php
+++ database/pagination.php
@@ -1,7 +1,5 @@
-<?php 
-    //include "html/affichage.php";
-
-    function pagination($connect){        
+<?php
+    function pagination($connect){
         try{
             $totalArticle = $connect->query("SELECT COUNT(id_article) FROM article")->fetchColumn();
             $limit = 10;
@@ -19,31 +17,14 @@
             $start = $offset + 1;
             $end = min(($offset + $limit), $totalArticle);
 
-
-            /*$listdebut = '<li class="page-item">';
-            $listSuite = ($page > 1) ?
-                       ' <a class="page-link" href="?page='.($page - 1).'" title="Previous page" aria-label="Previous">' :
-                            '<span aria-hidden="true">&laquo;</span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="?page=1">&laquo;</a></li>
-                    <li class="page-item">';
-            $listFin =  ($page < $pages) ? '<a class="page-link" href="?page=' . ($page + 1) . '" title="Next page" aria-label="Next">' :
-                            '<span class="disabled" aria-hidden="true">&raquo;</span>
-                            <span class="disabled sr-only">Next</span>
-                        </a>
-                    </li>';
-
-                    echo $listdebut.$listSuite.$listFin;*/
-
+            //Peret de changer de page en y incluant leur titre et n°
            $previousLink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a>
-            <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : 
+            <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' :
             '<span class="disabled">&laquo;</span>
             <span class="disabled">&lsaquo;</span>';
 
             $nextLink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a>
-            <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' : 
+            <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' :
             '<span class="disabled">&rsaquo;</span>
             <span class="disabled">&raquo;</span>';
 
--- database/selectDB.php
+++ database/selectDB.php
@@ -7,9 +7,9 @@
             FROM article
             INNER JOIN auteur ON article.id_auteur=auteur.id_auteur
             INNER JOIN categorie ON article.id_categorie=categorie.id_categorie
-            ORDER BY date DESC 
-            LIMIT 
-                :limit 
+            ORDER BY date DESC
+            LIMIT
+                :limit
             OFFSET
                 :offset");
 
@@ -75,7 +75,77 @@
         }
         catch(PDOExeption $e){
             echo "Request failed : " . $e->getMessage();
-        }        
+        }
+    }
+
+    function formulaire($connect){
+
+    //récupération des valeurs des champs:
+        $nom = $_POST["nom"];
+        $email = strtolower($_POST["email"]);
+        $titremessage = $_POST["titremessage"];
+        $message = $_POST["message"];
+        $categorie = $_POST["categorie"];
+
+        if(isset($_FILES['fichier']['name'])){
+            $fichier = $_FILES['fichier']['name'];
+        }
+        else{
+            $fichier = "";
+        }
+
+        if (isset($_POST["nom"])) {
+            echo 'Cette variable existe, donc je peux l\'afficher.';
+        }
+
+        try{
+            $sql = "INSERT INTO auteur (nom_auteur, mail)
+                VALUES ('$nom', '$email')";
+                // use exec() because no results are returned
+            $connect->query($sql);
+            echo "Nouvel auteur enregistré";
+
+            $sql = "SELECT id_auteur FROM auteur WHERE mail='$email'";
+            $resultat = $connect->query($sql);
+            $row=$resultat->fetch();
+
+            $sql = "INSERT INTO article (id_auteur, id_categorie, date, titre, texte, url_img)
+            VALUES ('".$row['id_auteur']."', '$categorie', NOW(), '$titremessage','$message','$fichier')";
+            // use exec() because no results are returned
+            $connect->exec($sql);
+            echo "Nouveau post enregistré";
+            }
+        catch(PDOException $e){
+            echo "Request failed : " . $e->getMessage();
+            }
+    }
+
+    function fichier(){
+        //Gère la partie upload image
+        
+            $target_dir = "../sources/images/";
+            $target_file = $target_dir .basename ($_FILES["fichier"]["name"]);
+
+        try {
+
+            if (file_exists($target_file)) {
+                $err= "Ce nom de fichier est déjà dans la base.";
+                return $err;
+            }
+
+            if(move_uploaded_file($_FILES['fichier']['tmp_name'], $target_file)) {
+                echo "L'image ".  basename( $_FILES['fichier']['name']).
+                "a bien été chargé!";
+            } else{
+                echo "Le fichier n'a pas était chargé! Merci de réessayer";
+            }
+
+            header("Location: ../index.php");
+
+        } catch (\Exception $e) {
+                echo "Request failed : " . $e->getMessage();
+
+        }
     }
 
 ?>
